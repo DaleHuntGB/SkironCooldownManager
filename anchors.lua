92b91513250eb428cfe3c082c55f918d28db7159
Permok <martinw1394@gmail.com>
2026-03-03 05:37:22 +0100

    Refactor 2

---
+++ anchors.lua
@@ -0,0 +1,315 @@
+local SCM = select(2, ...)
+
+local function OnResourceBarWidthChanged(self)
+	UIParent.SetWidth(self, self.SCMWidth)
+end
+
+function SCM:UpdateResourceBarWidth(maxGroupWidth)
+	for _, resourceBarName in ipairs(SCM.db.global.options.resourceBars) do
+		local resourceBar = _G[resourceBarName]
+		if resourceBar and resourceBar:IsShown() then
+			resourceBar.SCMWidth = max(200, maxGroupWidth)
+			resourceBar:SetWidth(max(200, maxGroupWidth))
+
+			if not resourceBar.SCMHook then
+				resourceBar.SCMHook = true
+				hooksecurefunc(resourceBar, "SetWidth", OnResourceBarWidthChanged)
+				hooksecurefunc(resourceBar, "SetSize", OnResourceBarWidthChanged)
+			end
+		end
+	end
+end
+
+function SCM:UpdateUUFValues(options, maxGroupWidth, rowConfig)
+	local offset = min((maxGroupWidth - 150), 0)
+	local mainAnchor = SCM:GetAnchor(1)
+
+	if UUF_Player then
+		if options.anchorUUF and options.anchorUUFRoles[(select(5, GetSpecializationInfo(GetSpecialization())))] then
+			if not UUF_Player.SCMOriginalAnchor then
+				UUF_Player.SCMOriginalAnchor = { UUF_Player:GetPoint() }
+				UUF_Player.SCMOriginalWidth = UUF_Player:GetWidth()
+				UUF_Player.SCMOriginalHeight = UUF_Player:GetHeight()
+			end
+			UUF_Player:ClearAllPoints()
+
+			mainAnchor.SetPoint(UUF_Player, "TOPRIGHT", mainAnchor, "TOPLEFT", offset, 0)
+
+			UUF_Player.SCMOffset = offset
+			UUF_Player.SCMHeight = rowConfig[1].size
+			UUF_Player.SCMAnchor = mainAnchor
+			UUF_Player.SCMCustomAnchor = true
+
+			UUF_Player:SetHeight(rowConfig[1].size)
+			UUF_Player_HealthBar:SetHeight(rowConfig[1].size - 2)
+			UUF_Player_HealthBackground:SetHeight(rowConfig[1].size - 2)
+
+			if not UUF_Player.SCMHook then
+				UUF_Player.SCMHook = true
+				hooksecurefunc(UUF_Player, "SetPoint", function(self)
+					if options.anchorUUF and options.anchorUUFRoles[(select(5, GetSpecializationInfo(GetSpecialization())))] then
+						self.SCMAnchor.SetPoint(self, "TOPRIGHT", self.SCMAnchor, "TOPLEFT", self.SCMOffset, 0)
+						self.SCMAnchor.SetHeight(self, self.SCMHeight)
+						self.SCMAnchor.SetHeight(UUF_Player_HealthBar, self.SCMHeight - 2)
+						self.SCMAnchor.SetHeight(UUF_Player_HealthBackground, self.SCMHeight - 2)
+					end
+				end)
+
+				hooksecurefunc(UUF_Player, "SetSize", function(self)
+					if options.anchorUUF and options.anchorUUFRoles[(select(5, GetSpecializationInfo(GetSpecialization())))] then
+						self.SCMAnchor.SetHeight(self, self.SCMHeight)
+						self.SCMAnchor.SetHeight(UUF_Player_HealthBar, self.SCMHeight - 2)
+						self.SCMAnchor.SetHeight(UUF_Player_HealthBackground, self.SCMHeight - 2)
+					end
+				end)
+			end
+		elseif UUF_Player.SCMCustomAnchor then
+			UUF_Player:ClearAllPoints()
+			UUF_Player.SCMAnchor.SetPoint(UUF_Player, unpack(UUF_Player.SCMOriginalAnchor))
+			UUF_Player.SCMAnchor.SetHeight(UUF_Player, UUF_Player.SCMOriginalHeight)
+			UUF_Player.SCMAnchor.SetHeight(UUF_Player_HealthBar, UUF_Player.SCMOriginalHeight - 2)
+			UUF_Player.SCMAnchor.SetHeight(UUF_Player_HealthBackground, UUF_Player.SCMOriginalHeight - 2)
+
+			UUF_Player.SCMCustomAnchor = nil
+			UUF_Player.SCMOffset = nil
+			UUF_Player.SCMHeight = nil
+			UUF_Player.SCMAnchor = nil
+			UUF_Player.SCMOriginalHeight = nil
+			UUF_Player.SCMOriginalAnchor = nil
+		end
+	end
+
+	if UUF_Target then
+		if options.anchorUUF and options.anchorUUFRoles[(select(5, GetSpecializationInfo(GetSpecialization())))] then
+			if not UUF_Target.SCMOriginalAnchor then
+				UUF_Target.SCMOriginalAnchor = { UUF_Target:GetPoint() }
+				UUF_Target.SCMOriginalWidth = UUF_Target:GetWidth()
+				UUF_Target.SCMOriginalHeight = UUF_Target:GetHeight()
+			end
+
+			UUF_Target:ClearAllPoints()
+			mainAnchor.SetPoint(UUF_Target, "TOPLEFT", mainAnchor, "TOPRIGHT", -offset, 0)
+
+			UUF_Target.SCMOffset = -offset
+			UUF_Target.SCMHeight = rowConfig[1].size
+			UUF_Target.SCMAnchor = mainAnchor
+			UUF_Target.SCMCustomAnchor = true
+
+			UUF_Target:SetHeight(rowConfig[1].size)
+			UUF_Target_HealthBar:SetHeight(rowConfig[1].size - 2)
+			UUF_Target_HealthBackground:SetHeight(rowConfig[1].size - 2)
+
+			if not UUF_Target.SCMHook then
+				UUF_Target.SCMHook = true
+				hooksecurefunc(UUF_Target, "SetPoint", function(self)
+					if options.anchorUUF and options.anchorUUFRoles[(select(5, GetSpecializationInfo(GetSpecialization())))] then
+						self.SCMAnchor.SetPoint(self, "TOPLEFT", self.SCMAnchor, "TOPRIGHT", self.SCMOffset, 0)
+						self.SCMAnchor.SetHeight(self, self.SCMHeight)
+						self.SCMAnchor.SetHeight(UUF_Target_HealthBar, self.SCMHeight - 2)
+						self.SCMAnchor.SetHeight(UUF_Target_HealthBackground, self.SCMHeight - 2)
+					end
+				end)
+
+				hooksecurefunc(UUF_Target, "SetSize", function(self)
+					if options.anchorUUF and options.anchorUUFRoles[(select(5, GetSpecializationInfo(GetSpecialization())))] then
+						self.SCMAnchor.SetHeight(self, self.SCMHeight)
+						self.SCMAnchor.SetHeight(UUF_Target_HealthBar, self.SCMHeight - 2)
+						self.SCMAnchor.SetHeight(UUF_Target_HealthBackground, self.SCMHeight - 2)
+					end
+				end)
+			end
+		elseif UUF_Target.SCMCustomAnchor then
+			UUF_Target:ClearAllPoints()
+			UUF_Target.SCMAnchor.SetPoint(UUF_Target, unpack(UUF_Target.SCMOriginalAnchor))
+			UUF_Target.SCMAnchor.SetHeight(UUF_Target, UUF_Target.SCMOriginalHeight)
+			UUF_Target.SCMAnchor.SetHeight(UUF_Target_HealthBar, UUF_Target.SCMOriginalHeight - 2)
+			UUF_Target.SCMAnchor.SetHeight(UUF_Target_HealthBackground, UUF_Target.SCMOriginalHeight - 2)
+
+			UUF_Target.SCMCustomAnchor = nil
+			UUF_Target.SCMOffset = nil
+			UUF_Target.SCMHeight = nil
+			UUF_Target.SCMAnchor = nil
+			UUF_Target.SCMOriginalHeight = nil
+			UUF_Target.SCMOriginalAnchor = nil
+		end
+	end
+
+	if ElvUF_Player then
+		if options.anchorElVUI and options.anchorElVUIRoles[(select(5, GetSpecializationInfo(GetSpecialization())))] then
+			if not ElvUF_Player.SCMOriginalAnchor then
+				ElvUF_Player.SCMOriginalAnchor = { ElvUF_Player:GetPoint() }
+				ElvUF_Player.SCMOriginalWidth = ElvUF_Player:GetWidth()
+				ElvUF_Player.SCMOriginalHeight = ElvUF_Player:GetHeight()
+			end
+
+			ElvUF_Player:ClearAllPoints()
+			mainAnchor.SetPoint(ElvUF_Player, "TOPRIGHT", mainAnchor, "TOPLEFT", offset, 0)
+
+			ElvUF_Player.SCMOffset = offset
+			ElvUF_Player.SCMHeight = rowConfig[1].size
+			ElvUF_Player.SCMAnchor = mainAnchor
+
+			ElvUF_Player:SetHeight(rowConfig[1].size)
+			ElvUF_Player_HealthBar:SetHeight(rowConfig[1].size - 2)
+			--ElvUF_Player_HealthBackground:SetHeight(rowConfig[1].size - 2)
+
+			if not ElvUF_Player.SCMHook then
+				ElvUF_Player.SCMHook = true
+				hooksecurefunc(ElvUF_Player, "SetPoint", function(self)
+					if options.anchorElvUF then
+						self.SCMAnchor.SetPoint(self, "TOPRIGHT", self.SCMAnchor, "TOPLEFT", self.SCMOffset, 0)
+						self.SCMAnchor.SetHeight(self, self.SCMHeight)
+						self.SCMAnchor.SetHeight(ElvUF_Player_HealthBar, self.SCMHeight - 2)
+					end
+				end)
+
+				hooksecurefunc(ElvUF_Player, "SetSize", function(self)
+					if options.anchorElvUF then
+						self.SCMAnchor.SetHeight(self, self.SCMHeight)
+						self.SCMAnchor.SetHeight(ElvUF_Player_HealthBar, self.SCMHeight - 2)
+					end
+				end)
+			end
+		elseif ElvUF_Player.SCMCustomAnchor then
+			ElvUF_Player:ClearAllPoints()
+			ElvUF_Player.SCMAnchor.SetPoint(ElvUF_Player, unpack(ElvUF_Player.SCMOriginalAnchor))
+			ElvUF_Player.SCMAnchor.SetHeight(ElvUF_Player, ElvUF_Player.SCMOriginalHeight)
+			ElvUF_Player.SCMAnchor.SetHeight(ElvUF_Player_HealthBar, ElvUF_Player.SCMOriginalHeight - 2)
+
+			ElvUF_Player.SCMCustomAnchor = nil
+			ElvUF_Player.SCMOffset = nil
+			ElvUF_Player.SCMHeight = nil
+			ElvUF_Player.SCMAnchor = nil
+			ElvUF_Player.SCMOriginalHeight = nil
+			ElvUF_Player.SCMOriginalAnchor = nil
+		end
+	end
+
+	if ElvUF_Target then
+		if options.anchorElVUI and options.anchorElVUIRoles[(select(5, GetSpecializationInfo(GetSpecialization())))] then
+			if not ElvUF_Target.SCMOriginalAnchor then
+				ElvUF_Target.SCMOriginalAnchor = { ElvUF_Target:GetPoint() }
+				ElvUF_Target.SCMOriginalWidth = ElvUF_Target:GetWidth()
+				ElvUF_Target.SCMOriginalHeight = ElvUF_Target:GetHeight()
+			end
+
+			ElvUF_Target:ClearAllPoints()
+			mainAnchor.SetPoint(ElvUF_Target, "TOPLEFT", mainAnchor, "TOPRIGHT", -offset, 0)
+
+			ElvUF_Target.SCMOffset = -offset
+			ElvUF_Target.SCMHeight = rowConfig[1].size
+			ElvUF_Target.SCMAnchor = mainAnchor
+
+			ElvUF_Target:SetHeight(rowConfig[1].size)
+			ElvUF_Target_HealthBar:SetHeight(rowConfig[1].size - 2)
+			--ElvUF_Target_HealthBackground:SetHeight(rowConfig[1].size - 2)
+
+			if not ElvUF_Target.SCMHook then
+				ElvUF_Target.SCMHook = true
+				hooksecurefunc(ElvUF_Target, "SetPoint", function(self)
+					if options.anchorElvUF then
+						self.SCMAnchor.SetPoint(self, "TOPLEFT", self.SCMAnchor, "TOPRIGHT", self.SCMOffset, 0)
+						self.SCMAnchor.SetHeight(self, self.SCMHeight)
+						self.SCMAnchor.SetHeight(ElvUF_Target_HealthBar, self.SCMHeight - 2)
+					end
+				end)
+
+				hooksecurefunc(ElvUF_Target, "SetSize", function(self)
+					if options.anchorElvUF then
+						self.SCMAnchor.SetHeight(self, self.SCMHeight)
+						self.SCMAnchor.SetHeight(ElvUF_Target_HealthBar, self.SCMHeight - 2)
+					end
+				end)
+			end
+		elseif ElvUF_Target.SCMCustomAnchor then
+			ElvUF_Target:ClearAllPoints()
+			ElvUF_Target.SCMAnchor.SetPoint(ElvUF_Target, unpack(ElvUF_Target.SCMOriginalAnchor))
+			ElvUF_Target.SCMAnchor.SetHeight(ElvUF_Target, ElvUF_Target.SCMOriginalHeight)
+			ElvUF_Target.SCMAnchor.SetHeight(ElvUF_Target_HealthBar, ElvUF_Target.SCMOriginalHeight - 2)
+
+			ElvUF_Target.SCMCustomAnchor = nil
+			ElvUF_Target.SCMOffset = nil
+			ElvUF_Target.SCMHeight = nil
+			ElvUF_Target.SCMAnchor = nil
+			ElvUF_Target.SCMOriginalHeight = nil
+			ElvUF_Target.SCMOriginalAnchor = nil
+		end
+	end
+end
+
+function SCM:ApplyCustomAnchors(maxGroupWidth, rowConfig)
+	for frame, options in pairs(self.CustomAnchors) do
+		frame = type(frame) == "string" and _G[frame] or frame
+		if frame and type(frame) == "table" and options.anchorIndex and options.xOffset and options.yOffset then
+			if not frame.SCMHook then
+				frame.SCMHook = true
+				frame.OriginalClearAllPoints = frame.ClearAllPoints
+				frame.OriginalSetPoint = frame.SetPoint
+				frame.ClearAllPoints = nop
+				frame.SetPoint = nop
+
+				if options.setWidth then
+					frame.OriginalSetWidth = frame.SetWidth
+					frame.SetWidth = nop
+				end
+			end
+
+			frame:OriginalClearAllPoints()
+			local point = options.point
+			local anchorRef = options.anchorFrame
+			local relativePoint = options.relativePoint
+			local xOffset = options.xOffset
+			local yOffset = options.yOffset
+
+			if point and anchorRef and relativePoint then
+				local setPoint = frame.OriginalSetPoint
+				local anchorRefType = type(anchorRef)
+				local isAnchorList = anchorRefType == "table"
+
+				if isAnchorList then
+					for i = 1, #anchorRef do
+						local ref = anchorRef[i]
+						local anchor
+						local anchorIndex = tonumber(ref)
+						if anchorIndex then
+							anchor = SCM:GetAnchor(anchorIndex)
+						else
+							local refType = type(ref)
+							if refType == "string" then
+								anchor = _G[ref]
+							elseif refType == "table" then
+								anchor = ref
+							end
+						end
+
+						if anchor and anchor:IsVisible() then
+							setPoint(frame, point, anchor, relativePoint, xOffset, yOffset)
+							break
+						end
+					end
+				else
+					local anchor
+					local anchorIndex = tonumber(anchorRef)
+					if anchorIndex then
+						anchor = SCM:GetAnchor(anchorIndex)
+					elseif anchorRefType == "string" then
+						anchor = _G[anchorRef]
+					elseif anchorRefType == "table" then
+						anchor = anchorRef
+					end
+
+					if anchor and anchor:IsVisible() then
+						setPoint(frame, point, anchor, relativePoint, xOffset, yOffset)
+						break
+					end
+				end
+			else
+				frame:OriginalSetPoint("BOTTOM", SCM:GetAnchor(options.anchorIndex), "TOP", options.xOffset, options.yOffset)
+			end
+
+			if options.setWidth then
+				frame:OriginalSetWidth(max(200, maxGroupWidth - (options.widthOffset or 0)))
+			end
+		end
+	end
+end
