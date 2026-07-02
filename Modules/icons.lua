abd30f3430deec0d66a84292802af15755bb32a1
Permok <martinw1394@gmail.com>
2026-07-03 00:21:09 +0200

    Fix GCD of custom icon slots if the option is not enabled

--- Modules/icons.lua
+++ Modules/icons.lua
@@ -354,7 +354,6 @@ local function ProcessBuffIcon(child, childData, options)
 
 	local isInactive
 	if not issecretvalue(child.isActive) then
-		print("PROCESS", child.isActive, child.SCMSpellID, C_Spell.GetSpellName(child.SCMSpellID))
 		isInactive = not child.isActive
 	elseif child.SCMCheckCooldownFrame then
 		isInactive = not child.Cooldown:IsVisible() and not child.SCMFixedDuration
