c362170cae25e4111ec15776579ff9e5c686b36d
Permok <martinw1394@gmail.com>
2026-08-10 22:24:25 +0200

    Make sure other addons can't import profiles until SCM is initialized

--- api.lua
+++ api.lua
@@ -26,14 +26,26 @@ end
 --   includeGlobalSettings: import global settings.
 --   includeGlobalAnchors: import global anchors.
 function SCMAPI.ImportProfile(profileName, importString, importSettings)
+	if not SCM.initialized then
+		return
+	end
+
 	SCM:ImportProfile(profileName, importString, importSettings)
 end
 
 function SCMAPI.ImportGlobalSettings(importString)
+	if not SCM.initialized then
+		return
+	end
+
 	SCM:ImportGlobalSettings(importString)
 end
 
 function SCMAPI.ImportGlobalAnchors(importString)
+	if not SCM.initialized then
+		return
+	end
+
 	SCM:ImportGlobalAnchors(importString)
 end
 
@@ -48,6 +60,10 @@ function SCMAPI.AddCustomIcon(anchorGroup, iconType, configID, order, uniqueID, 
 end
 
 function SCMAPI.RegisterAndLoadConfig(profileName, classFileName, config, specID)
+	if not SCM.initialized then
+		return
+	end
+
 	SCM.DB:RegisterAndLoadConfig(profileName, classFileName, config, specID)
 end
 
